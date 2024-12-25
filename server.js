8bb26ce5d399b2ad78baf4ae17ea408a6ee07492
lokesh-wagh <lokeshwagh004@gmail.com>
2024-12-25 17:17:12 +0530

    new backend

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const cors = require('cors');  // Import the cors package
+
 const app = express();
 const port = 3000;
+
+var arr = [];
+
+// Enable CORS for all routes
+app.use(cors());  // This will allow requests from any origin by default
+app.use(express.json());
+// Alternatively, you can configure CORS for specific origins:
+app.use(cors({
+  origin: 'http://localhost:5173',  // Replace with your frontend URL
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
 
 app.get('/', (req, res) => {
     res.send(`
@@ -10,14 +24,21 @@ app.get('/', (req, res) => {
             <title>Express HTML</title>
         </head>
         <body>
-            <a>
-                https://www.youtube.com/watch?v=dQw4w9WgXcQg
-            </a>
-            <b>this text is new addition to test the script</b>
-            i hope this changes
+           this is root of the backend
         </body>
         </html>
     `);
+});
+
+app.get('/members', (req, res) => {
+    res.send(arr);
+});
+
+app.post('/add', (req, res) => {
+    console.log(req.body)
+    const new_member = req.body.name;
+    arr = [...arr, new_member];
+    res.send("successfully added");
 });
 
 app.listen(port, () => {
